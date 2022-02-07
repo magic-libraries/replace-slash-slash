0654a3bc1470a684e1243ac50123044841487444
jascha <j@jaeh.at>
2022-02-07 22:34:10 +0100

    format

--- test/spec.mjs
+++ test/spec.mjs
@@ -2,9 +2,10 @@ import { is } from '@magic/test'
 
 import defaultReplaceSlashSlash, { replaceSlashSlash } from '../src/index.mjs'
 
-
 export default [
   { fn: true },
-  { fn: is.deep.equal(defaultReplaceSlashSlash, replaceSlashSlash), info: 'default export equals named export' },
-
+  {
+    fn: is.deep.equal(defaultReplaceSlashSlash, replaceSlashSlash),
+    info: 'default export equals named export',
+  },
 ]
