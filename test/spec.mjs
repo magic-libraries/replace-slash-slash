c5ce4b2df4fa869f30a195c5074dda0ea7b6038f
jascha <j@jaeh.at>
2022-02-07 22:39:44 +0100

    test/spec: add @magic/test.version

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,14 @@
-import { is } from '@magic/test'
+import { is, version } from '@magic/test'
 
 import defaultReplaceSlashSlash, { replaceSlashSlash } from '../src/index.mjs'
 
+const spec = {
+  default: 'fn',
+  replaceSlashSlash: 'fn',
+}
+
 export default [
-  { fn: true },
+  ...version({ replaceSlashSlash }, spec),
   {
     fn: is.deep.equal(defaultReplaceSlashSlash, replaceSlashSlash),
     info: 'default export equals named export',
